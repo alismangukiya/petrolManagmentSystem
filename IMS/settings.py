1e4fa9fbc0410e189fafee12c935e5917c14e14b
Alis Mangukiya <47888031+alismangukiya@users.noreply.github.com>
2022-01-19 10:05:26 +0530

    Update settings.py

--- IMS/settings.py
+++ IMS/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "hackcode0001@gmail.com"
-EMAIL_HOST_PASSWORD = "lbwrvmmdwscxcgyc"
+EMAIL_HOST_USER = "your mail"
+EMAIL_HOST_PASSWORD = "your password"
